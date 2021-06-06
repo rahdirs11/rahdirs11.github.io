4fb6b983889a0da1db8a6ea994100ea71cbb152c
Sridhar N S <iamsridhar11@gmail.com>
2021-06-06 13:57:01 +0530

    My new Portfolio

---
+++ script.js
@@ -0,0 +1,21 @@
+var tributePage = document.getElementById("tributePage");
+var landingPage = document.getElementById("landingPage");
+var surveyForm = document.getElementById("surveyForm");
+var technicalDocumentation = document.getElementById("technicalDocumentation");
+
+
+tributePage.addEventListener("click", function() {
+    window.open("https://rahdirs11.github.io/freeCodeCamp_TributePage/", "_blank");
+})
+
+landingPage.addEventListener('click', () => {
+    window.open("https://rahdirs11.github.io/freeCodeCamp_LandingPage/", "_blank")
+})
+
+surveyForm.addEventListener('click', () => {
+    window.open("https://rahdirs11.github.io/freeCodeCamp_SurveyForm", '_blank')
+})
+
+technicalDocumentation.addEventListener('click', () => {
+    window.open("https://rahdirs11.github.io/freeCodeCamp_TechnicalDocumentation/", '_blank')
+})
